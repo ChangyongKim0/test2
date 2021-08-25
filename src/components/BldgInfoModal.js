279b8112584a38fb3429348968628d877aae8ac3
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-08-25 21:11:51 +0900

    Valuation Page Created

--- src/components/BldgInfoModal.js
+++ src/components/BldgInfoModal.js
@@ -1,18 +1,25 @@
 import React, { useEffect } from "react";
+import BldgInfoModalContent from "./BldgInfoModalContent";
 
 import styles from "./BldgInfoModal.module.scss";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
-const BackgroundMap = () => {
-  useEffect(() => {});
-
+const BldgInfoModal = ({ info }) => {
+  useEffect(() => {
+    // console.log(info.keys);
+  });
+  if (info.show === false) {
+    return <></>;
+  }
   return (
-    <div className="wrapper">
-      <div id="map" className={cx("map")}></div>
+    <div className={cx("wrapper")}>
+      <div className={cx("modal")}>
+        <BldgInfoModalContent keys={info.keys} vals={info.vals} />
+      </div>
     </div>
   );
 };
 
-export default BackgroundMap;
+export default BldgInfoModal;
