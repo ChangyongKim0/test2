f765141c964cdcf787c8e9f1c79f02e34a53ffd0
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-08-25 17:05:14 +0900

    updating valuation and map

---
+++ src/components/BackgroundMap.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+
+import styles from "./BackgroundMap.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+const BackgroundMap = () => {
+  useEffect(() => {
+    var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    console.log(container);
+    var options = {
+      // //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(37.5642135, 127.0016985), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    var map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    console.log(window.kakao);
+  });
+
+  return (
+    <div className={cx("wrapper")}>
+      <div id="map" className={cx("map")}></div>
+    </div>
+  );
+};
+
+export default BackgroundMap;
