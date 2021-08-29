e2f885bf7f477671882c1624841b7b60548348ba
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-08-30 06:38:10 +0900

    data updated

--- src/components/BackgroundMap.js
+++ src/components/BackgroundMap.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 
 import styles from "./BackgroundMap.module.scss";
 import classNames from "classnames/bind";
@@ -19,15 +20,22 @@ const BackgroundMap = ({ handleBldgInfo, handleAddress }) => {
     window.kakao.maps.event.addListener(map, "click", onClick);
     window.kakao.maps.event.addListener(map, "center_changed", onCenterChanged);
     getAddress(handleAddress);
-    let area = getGeoData();
-    console.log(area);
-    displayGeoData(area);
+    axios
+      .get(
+        "https://raw.githubusercontent.com/ChangyongKim0/test2/master/src/data/junggu.json"
+      )
+      .then((response) => {
+        let data_list = response.data;
+        console.log(data_list);
+        for (let data in data_list) {
+          let area = convertGeoData(data."gml:posList");
+          displayGeoData(area);
+        }
+      });
   }, []);
 
-  const getGeoData = () => {
+  const convertGeoData = (data) => {
     let polygon = [];
-    let data =
-      "126.98371459 37.56675117 126.98368912 37.56674981 126.98368743 37.56673143 126.98359122 37.56674034 126.98349557 37.56674402 126.98338295 37.56672237 126.98338352 37.56671066 126.98329354 37.56669614 126.9831113 37.56670702 126.98311073 37.56671197 126.98311979 37.56671738 126.983107 37.56671747 126.98310564 37.56670512 126.98309207 37.56667989 126.98308758 37.56652492 126.98308894 37.56650051 126.98327551 37.56637178 126.98337287 37.56630377 126.98345551 37.56625018 126.98352343 37.56622361 126.98358795 37.56621416 126.98379843 37.56621331 126.98391053 37.56621286 126.98391211 37.56673723 126.98391097 37.5667512 126.98389456 37.56675345 126.98388721 37.56673903 126.98385891 37.56674128 126.98385551 37.56675299 126.98371459 37.56675117";
     let data_list = data.split(" ");
     console.log(data_list);
     for (let i = 0; i < data_list.length; ) {
