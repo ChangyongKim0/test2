a4f91fee89d1d8035941edb1c4b30f0d06e9d376
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-05 06:07:36 +0000

    detail framework modified

--- src/components/LandDataCompText.js
+++ src/components/LandDataCompText.js
@@ -7,10 +7,16 @@ import ToolTip from "./ToolTip";
 
 const cx = classNames.bind(styles);
 
-const LandDataCompText = ({ data, use_tooltip, tooltip, style }) => {
+const LandDataCompText = ({
+  data,
+  use_tooltip,
+  tooltip,
+  style,
+  force_use_tooltip,
+}) => {
   return (
     <div className={cx("wrapper")}>
-      <ToolTip enable={use_tooltip} data={tooltip}>
+      <ToolTip enable={force_use_tooltip || use_tooltip} data={tooltip}>
         <div className={cx("frame")}>
           <div className={cx("text")}>{data.value}</div>
           <div className={cx("unit")}>{data.unit}</div>
@@ -29,6 +35,7 @@ LandDataCompText.defaultProps = {
   use_tooltip: false,
   tooltip: ["tooltip[0]", "tooltip[1]"],
   style: "default",
+  force_use_tooltip: false,
 };
 
 export default LandDataCompText;
