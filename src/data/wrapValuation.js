1c2b9dd1d2d63c75d062849b3b0f1f638fd6081c
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-08 12:12:51 +0000

    valuation wrapper created

---
+++ src/data/wrapValuation.js
@@ -0,0 +1,850 @@
+export let formatted_data = {
+  base: {
+    land_area: {
+      base: "",
+      value: "1,070",
+      base_type: "",
+      value_type: "number_detail",
+    },
+    use_area: {
+      base: "",
+      value: "도시지역, 일반상업지역",
+      base_type: "",
+      value_type: "string",
+    },
+    recent_trade: {
+      base: "19.4Q",
+      value: "2,000억",
+      base_type: "string",
+      value_type: "number",
+    },
+    land_price_per_py: {
+      base: "",
+      value: "6.9억",
+      base_type: "",
+      value_type: "number",
+    },
+    floor_price_per_py: {
+      base: "",
+      value: "1,575만",
+      base_type: "",
+      value_type: "number",
+    },
+    noc: {
+      base: "",
+      value: "23.7만",
+      base_type: "",
+      value_type: "number",
+    },
+    opex: {
+      base: "",
+      value: "20만 / 4.7만",
+      base_type: "",
+      value_type: "string",
+    },
+    floor_area: {
+      base: "",
+      value: "4,919",
+      base_type: "",
+      value_type: "number_detail",
+    },
+    far: {
+      base: "",
+      value: "459",
+      base_type: "",
+      value_type: "rate_over",
+    },
+    bcr_par: {
+      base: "",
+      value: "47 / 58",
+      base_type: "",
+      value_type: "string",
+    },
+    size: {
+      base: "",
+      value: "20F / B6",
+      base_type: "",
+      value_type: "specific",
+    },
+    const_year: {
+      base: "",
+      value: "2019",
+      base_type: "",
+      value_type: "string",
+    },
+  },
+  archi: {
+    bcr: {
+      base: "60",
+      value: "642",
+      base_type: "rate",
+      value_type: "number_detail",
+    },
+    far: {
+      base: "800",
+      value: "8,580",
+      base_type: "rate_over",
+      value_type: "number_detail",
+    },
+    par: {
+      base: "60",
+      value: "5,136",
+      base_type: "rate",
+      value_type: "number_detail",
+    },
+    high_floor: {
+      base: "오피스",
+      value: "3F-20F",
+      base_type: "string",
+      value_type: "floor_range",
+    },
+    hf_area: {
+      base: "428",
+      value: "7,704",
+      base_type: "number_detail",
+      value_type: "number_detail",
+    },
+    hf_parea: {
+      base: "257",
+      value: "4,622",
+      base_type: "number_detail",
+      value_type: "number_detail",
+    },
+    low_floor: {
+      base: "상업",
+      value: "B1-2",
+      base_type: "string",
+      value_type: "floor_range",
+    },
+    lf_area: {
+      base: "428",
+      value: "1,284",
+      base_type: "number_detail",
+      value_type: "number_detail",
+    },
+    lf_parea: {
+      base: "257",
+      value: "770",
+      base_type: "number_detail",
+      value_type: "number_detail",
+    },
+  },
+  rent: {
+    noi: {
+      base: "",
+      value: "168.3억",
+      base_type: "",
+      value_type: "number",
+    },
+    hf_rent: {
+      base: "",
+      value: "27만",
+      base_type: "",
+      value_type: "number",
+    },
+    hf_opex: {
+      base: "4만",
+      value: "4만",
+      base_type: "number",
+      value_type: "number",
+    },
+    hf_noi: {
+      base: "33.7만",
+      value: "16.2만",
+      base_type: "number",
+      value_type: "number",
+    },
+    lf_rent: {
+      base: "",
+      value: "20만",
+      base_type: "",
+      value_type: "number",
+    },
+    lf_opex: {
+      base: "5만",
+      value: "5만",
+      base_type: "number",
+      value_type: "number",
+    },
+    lf_noi: {
+      base: "28.3만",
+      value: "12만",
+      base_type: "number",
+      value_type: "number",
+    },
+  },
+  use: {
+    tic: {
+      base: "",
+      value: "4,000억",
+      base_type: "",
+      value_type: "number",
+    },
+    buy_total: {
+      base: "",
+      value: "3,287억",
+      base_type: "",
+      value_type: "number",
+    },
+    buy_price: {
+      base: "8.2억",
+      value: "3,100억",
+      base_type: "number",
+      value_type: "number",
+    },
+    buy_support: {
+      base: "5.5",
+      value: "187억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    buy_fee: {
+      base: "1.0",
+      value: "40억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    brok_fee: {
+      base: "0.6",
+      value: "18.6억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    const_fee: {
+      base: "12억",
+      value: "24억",
+      base_type: "number",
+      value_type: "number",
+    },
+    am_pm_fee: {
+      base: "12억",
+      value: "24억",
+      base_type: "number",
+      value_type: "number",
+    },
+    buy_reserve: {
+      base: "12억",
+      value: "20억",
+      base_type: "number",
+      value_type: "number",
+    },
+    const_total: {
+      base: "",
+      value: "780억",
+      base_type: "",
+      value_type: "number",
+    },
+    const_price: {
+      base: "790만",
+      value: "644억",
+      base_type: "number",
+      value_type: "number",
+    },
+    indr_const_price: {
+      base: "80만",
+      value: "84.7억",
+      base_type: "number",
+      value_type: "number",
+    },
+    fin_total: {
+      base: "",
+      value: "350억",
+      base_type: "number",
+      value_type: "number",
+    },
+    pf_interest: {
+      base: "5.5",
+      value: "340억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    other_fee: {
+      base: "1.0",
+      value: "10억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    other_total: {
+      base: "",
+      value: "102.3억",
+      base_type: "",
+      value_type: "number",
+    },
+    ti_rent_fee: {
+      base: "200만",
+      value: "56.8억",
+      base_type: "number",
+      value_type: "number",
+    },
+    stable_reserve: {
+      base: "",
+      value: "55.5억",
+      base_type: "",
+      value_type: "number",
+    },
+  },
+  src: {
+    src_total: {
+      base: "",
+      value: "4,000억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    debt_interest: {
+      base: "",
+      value: "5.5",
+      base_type: "rate",
+      value_type: "number",
+    },
+    pf: {
+      base: "75",
+      value: "3,000억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    tr_a: {
+      base: "5.8",
+      value: "2,000억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    tr_b: {
+      base: "5.2",
+      value: "1,000억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    equity: {
+      base: "25",
+      value: "1,000억",
+      base_type: "rate",
+      value_type: "number",
+    },
+    pref_share: {
+      base: "",
+      value: "400억",
+      base_type: "",
+      value_type: "number",
+    },
+    common_share: {
+      base: "",
+      value: "600억",
+      base_type: "",
+      value_type: "number",
+    },
+  },
+  other: {
+    cap: {
+      base: "",
+      value: "3.9",
+      base_type: "",
+      value_type: "rate",
+    },
+    period_total: {
+      base: "",
+      value: "30",
+      base_type: "",
+      value_type: "number_detail",
+    },
+  },
+  result: {
+    sell_price: {
+      base: "",
+      value: "4,314억",
+      base_type: "",
+      value_type: "number",
+    },
+    return: {
+      base: "",
+      value: "114.2",
+      base_type: "",
+      value_type: "number",
+    },
+    roe: {
+      base: "",
+      value: "10.9",
+      base_type: "",
+      value_type: "rate_over",
+    },
+    irr: {
+      base: "",
+      value: "5.44",
+      base_type: "",
+      value_type: "rate_over",
+    },
+  },
+};
+
+export const default_data = {
+  id: 1,
+  use_mini_map: false,
+  mini_map: {
+    level: 3,
+    pos_list:
+      "127.02473058 37.49791889 127.02435588 37.49780526 127.02440471 37.4977032 127.02441654 37.49767838 127.02403691 37.49756322 127.02409308 37.49744545 127.02414913 37.49732768 127.0242053 37.49720991 127.02422135 37.49717621 127.0243247 37.49713961 127.02462926 37.49723307 127.02481451 37.49728998 127.02500339 37.49734796 127.02492292 37.49751638 127.02484788 37.49767344 127.02477937 37.49781679 127.02473058 37.49791889",
+  },
+  title: "title",
+  sub_title: "sub_title",
+  total_info: [
+    { value: "[0].value", unit: "[0].u" },
+    { value: "[1].value", unit: "[1].u" },
+  ],
+  data: [
+    [
+      {
+        data: {
+          title: "[0][0]title",
+          base: "base",
+          base_unit: "u.",
+          base_type: "rate",
+          value: "value",
+          value_unit: "u.",
+          value_type: "number",
+        },
+        is_placeholder: {
+          base: true,
+          value: true,
+        },
+        onEnterPress: {
+          Base: () => {},
+          Value: () => {},
+        },
+        type: "total",
+        use_tooltip: {
+          title: false,
+          base: false,
+          value: false,
+        },
+        tooltip: {
+          title: ["tooltip.title[0]", "tooltip.title[1]"],
+          base: ["tooltip.base[0]", "tooltip.base[1]"],
+          value: ["tooltip.value[0]", "tooltip.value[1]"],
+        },
+      },
+      {
+        data: {
+          title: "[0][1]title",
+          base: "base",
+          base_unit: "u.",
+          base_type: "rate",
+          value: "value",
+          value_unit: "u.",
+          value_type: "number",
+        },
+        is_placeholder: {
+          base: true,
+          value: true,
+        },
+        onEnterPress: {
+          Base: () => {},
+          Value: () => {},
+        },
+        type: "default",
+        use_tooltip: {
+          title: false,
+          base: false,
+          value: false,
+        },
+        tooltip: {
+          title: ["tooltip.title[0]", "tooltip.title[1]"],
+          base: ["tooltip.base[0]", "tooltip.base[1]"],
+          value: ["tooltip.value[0]", "tooltip.value[1]"],
+        },
+      },
+    ],
+    [
+      {
+        data: {
+          title: "[1][0]title",
+          base: "base",
+          base_unit: "u.",
+          base_type: "rate",
+          value: "value",
+          value_unit: "u.",
+          value_type: "number",
+        },
+        is_placeholder: {
+          base: true,
+          value: true,
+        },
+        onEnterPress: {
+          Base: () => {},
+          Value: () => {},
+        },
+        type: "total",
+        use_tooltip: {
+          title: false,
+          base: false,
+          value: false,
+        },
+        tooltip: {
+          title: ["tooltip.title[0]", "tooltip.title[1]"],
+          base: ["tooltip.base[0]", "tooltip.base[1]"],
+          value: ["tooltip.value[0]", "tooltip.value[1]"],
+        },
+      },
+      {
+        data: {
+          title: "[1][1]title",
+          base: "base",
+          base_unit: "u.",
+          base_type: "rate",
+          value: "value",
+          value_unit: "u.",
+          value_type: "number",
+        },
+        is_placeholder: {
+          base: true,
+          value: true,
+        },
+        onEnterPress: {
+          Base: () => {},
+          Value: () => {},
+        },
+        type: "default",
+        use_tooltip: {
+          title: false,
+          base: false,
+          value: false,
+        },
+        tooltip: {
+          title: ["tooltip.title[0]", "tooltip.title[1]"],
+          base: ["tooltip.base[0]", "tooltip.base[1]"],
+          value: ["tooltip.value[0]", "tooltip.value[1]"],
+        },
+      },
+    ],
+  ],
+  style: "white",
+  use_plus: true,
+  handlePlus: () => {},
+  force_use_tooltip: false,
+};
+
+const getSrcData = (formatted_data) => {
+  const x = formatted_data.src;
+  return {
+    id: "src",
+    title: "자금 가정",
+    sub_title: "필요 자금 총액 / 대출 이자율",
+    total_info: [
+      { value: x.src_total.value, unit: "원" },
+      { value: x.debt_interest.value, unit: "%" },
+    ],
+    data: [
+      [
+        {
+          data: {
+            ...x.pf,
+            id: "src.pf",
+            title: "PF 대출",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: false,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "total",
+          tooltip: {
+            title: [],
+            base: ["사업자금 총액 대비 비율"],
+            value: [],
+          },
+        },
+        {
+          data: {
+            ...x.tr_a,
+            id: "src.tr_a",
+            title: "Tr-A",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: {
+            title: [],
+            base: ["이자율"],
+            value: [],
+          },
+        },
+        {
+          data: {
+            ...x.tr_b,
+            id: "src.tr_b",
+            title: "Tr-B",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: {
+            title: [],
+            base: ["이자율"],
+            value: [],
+          },
+        },
+      ],
+      [
+        {
+          data: {
+            ...x.equity,
+            id: "src.equity",
+            title: "자본금",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: false,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "total",
+          tooltip: {
+            title: [],
+            base: ["사업자금 총액 대비 비율"],
+            value: [],
+          },
+        },
+        {
+          data: {
+            ...x.pref_share,
+            id: "src.prefer_share",
+            title: "우선주",
+            base_unit: "",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: false,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: { title: [], base: [], value: [] },
+        },
+        {
+          data: {
+            ...x.common_share,
+            id: "src.common_share",
+            title: "보통주",
+            base_unit: "",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: false,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: {
+            title: [],
+            base: [],
+            value: [],
+          },
+        },
+      ],
+    ],
+    style: "white",
+    use_plus: true,
+    handlePlus: () => {},
+  };
+};
+
+const getUseData = (formatted_data) => {
+  const x = formatted_data.use;
+  return {
+    id: "use",
+    title: "비용 가정",
+    sub_title: "토지 구매비 등 부대 비용 총합",
+    total_info: [{ value: x.tic.value, unit: "원" }],
+    data: [
+      [
+        {
+          data: {
+            ...x.buy_total,
+            id: "use.buy_total",
+            title: "매매비용 계",
+            base_unit: "",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: false,
+            value: false,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "total",
+          tooltip: {
+            title: [],
+            base: [],
+            value: [],
+          },
+        },
+        {
+          data: {
+            ...x.buy_price,
+            id: "use.buy_price",
+            title: "토지 구매비",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: {
+            title: [],
+            base: ["이자율"],
+            value: [],
+          },
+        },
+        {
+          data: {
+            ...x.tr_b,
+            id: "use.tr_b",
+            title: "Tr-B",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: {
+            title: [],
+            base: ["이자율"],
+            value: [],
+          },
+        },
+      ],
+      [
+        {
+          data: {
+            ...x.equity,
+            id: "use.equity",
+            title: "자본금",
+            base_unit: "\u00A0%",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: true,
+            value: false,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "total",
+          tooltip: {
+            title: [],
+            base: ["사업자금 총액 대비 비율"],
+            value: [],
+          },
+        },
+        {
+          data: {
+            ...x.pref_share,
+            id: "use.prefer_share",
+            title: "우선주",
+            base_unit: "",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: false,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: { title: [], base: [], value: [] },
+        },
+        {
+          data: {
+            ...x.common_share,
+            id: "use.common_share",
+            title: "보통주",
+            base_unit: "",
+            value_unit: "\u00A0원",
+          },
+          is_placeholder: {
+            base: false,
+            value: true,
+          },
+          onEnterPress: {
+            Base: () => {},
+            Value: () => {},
+          },
+          type: "default",
+          tooltip: {
+            title: [],
+            base: [],
+            value: [],
+          },
+        },
+      ],
+    ],
+    style: "white",
+    use_plus: true,
+    handlePlus: () => {},
+  };
+};
+
+const wrapValuation = (input_data) => {
+  const base = getUseData(input_data);
+  const archi = getUseData(input_data);
+  const rent = getUseData(input_data);
+  const use = getUseData(input_data);
+  const src = getSrcData(input_data);
+  const footer = getUseData(input_data);
+  return {
+    base: base,
+    archi: archi,
+    rent: rent,
+    use: use,
+    src: src,
+    footer: footer,
+  };
+};
+
+export default wrapValuation;
