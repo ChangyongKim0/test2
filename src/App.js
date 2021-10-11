f3063932dc979168e5cf50cb148686cb07753099
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-11 06:50:21 +0000

    useContext Provider wrapped

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import { ZIndexProvider } from "./functions/Zindexer";
+import { ValuationCalculatorProvider } from "./hooks/useValuationCalculator";
 import {
   About,
   TestPage,
@@ -13,19 +14,21 @@ import {
 class App extends Component {
   render() {
     return (
-      <ZIndexProvider>
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route path="/about" component={About} />
-        <Route exact path="/" component={Home} />
-        {/* <Route path="/login" component={Login} />
+      <ValuationCalculatorProvider>
+        <ZIndexProvider>
+          {/* <Route exact path="/" component={Home} /> */}
+          <Route path="/about" component={About} />
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/login" component={Login} />
         <Route path="/map" component={Map} />
         <Route exact path="/tips" component={Tips} />
         <Route path="/tips/new" component={NewTips} /> */}
-        <Route exact path="/valuation" component={Valuation} />
-        <Route path="/valuation/comp" component={ValuationComp} />
-        <Route path="/testpage" component={TestPage} />
-        <Route path="/map/comp" component={MapComp} />
-      </ZIndexProvider>
+          <Route exact path="/valuation" component={Valuation} />
+          <Route path="/valuation/comp" component={ValuationComp} />
+          <Route path="/testpage" component={TestPage} />
+          <Route path="/map/comp" component={MapComp} />
+        </ZIndexProvider>
+      </ValuationCalculatorProvider>
     );
   }
 }
