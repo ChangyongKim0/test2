f0c21e5abfcb1e959b1bd0f09301d4d4de260601
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-13 11:54:58 +0000

    deep copy created but not used

---
+++ src/hooks/useDeepCopy.js
@@ -0,0 +1,19 @@
+// const lodash = require("lodash");
+
+// function deepCopy(obj) {
+//   var clone = {};
+//   try {
+//     clone = {deepCopy()}
+//   } catch {
+//     for (var key in obj) {
+//       if (typeof obj[key] == "object" && obj[key] != null) {
+//         clone[key] = deepCopy(obj[key]);
+//       } else {
+//         clone[key] = obj[key];
+//       }
+//     }
+//     return clone;
+//   }
+// }
+
+// export default deepCopy;
