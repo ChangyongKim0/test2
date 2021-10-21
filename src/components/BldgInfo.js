af34f4ce52eb88a4d22279023e85018fbc96ff56
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-21 04:52:21 +0000

    Bldg info design modified

--- src/components/BldgInfo.js
+++ src/components/BldgInfo.js
@@ -3,10 +3,43 @@ import React, { useEffect, useState } from "react";
 import styles from "./BldgInfo.module.scss";
 import classNames from "classnames/bind";
 import ToolTip from "./ToolTip";
+import BldgInfoBlob from "./BldgInfoBlob";
+import BldgInfoText from "./BldgInfoText";
 
 const cx = classNames.bind(styles);
 
-const BldgInfo = ({ keys, vals }) => {
+const BldgInfo = ({
+  price,
+  noc,
+  land_price_per_area,
+  rent,
+  opex,
+  land_title,
+  land_size,
+  land_usage,
+  usage_list,
+  attachment_land,
+  bldg_title,
+  bldg_size,
+  bldg_year,
+  bldg_floor_area_ratio,
+  bldg_floor,
+  bldg_coverage_ratio,
+  bldg_exclusive_rate,
+  bldg_usage_list,
+  bldg_detail,
+  dev_title,
+  dev_floor_area_ratio,
+  dev_coverage_ratio,
+  dev_height,
+  dev_usage_list,
+  official_price_title,
+  official_price_year,
+  official_price,
+  siml_land_title,
+  siml_land_address,
+  siml_land_price,
+}) => {
   let [show_info, setShowInfo] = useState(false);
 
   useEffect(() => {
@@ -16,148 +49,549 @@ const BldgInfo = ({ keys, vals }) => {
 
   return (
     <div className={cx("wrapper")}>
-      <div className={cx("block")}>
-        <p className={cx("name", "emph")}>{vals.name}</p>
-        <p className={cx("addr1")}>{vals.addr1}</p>
-        <p className={cx("addr2")}>{vals.addr2}</p>
-      </div>
-      <div className={cx("block", "primary")}>
-        <div>
-          <p className={cx("keys")}>{keys.buy_price}</p>
-          <p className={cx("vals", "emph")}>{vals.buy_price}</p>
-          <p className={cx("vals")}>{vals.buy_price_date}</p>
+      <div className={cx("frame-section")}>
+        <div className={cx("frame-div2")}>
+          <BldgInfoText {...price} />
+          <BldgInfoText {...noc} />
         </div>
-        <div>
-          <div className={cx("with-info")}>
-            <p className={cx("keys")}>{keys.noc}</p>
-            <div className={cx("info-frame")}>
-              <p
-                className={cx("info")}
-                onMouseOver={() => setShowInfo(true)}
-                onMouseOut={() => setShowInfo(false)}
-              >
-                ⓘ
-              </p>
-              {}
-              <div className={cx("info-window")}>
-                <ToolTip
-                  info={{ active: show_info, content: "이것은 인포윈도우!" }}
-                />
-              </div>
-            </div>
-          </div>
-          <p className={cx("vals", "emph")}>{vals.noc}</p>
+        <div className={cx("frame-div4")}>
+          <BldgInfoText {...land_price_per_area} />
+          <div></div>
+          <BldgInfoText {...rent} />
+          <BldgInfoText {...opex} />
         </div>
       </div>
-      <div className={cx("block", "secondary")}>
-        <div>
-          <p className={cx("keys")}>{keys.land_area}</p>
-          <p className={cx("vals")}>{vals.land_area}</p>
+      <div className={cx("frame-section")}>
+        <div className={cx("frame-div4")}>
+          <BldgInfoText {...land_title} />
+          <div></div>
+          <BldgInfoText {...land_size} />
+          <BldgInfoText {...land_usage} />
         </div>
-        <div>
-          <p className={cx("keys")}>{keys.use_area}</p>
-          <p className={cx("vals")}>{vals.use_area}</p>
+        <div className={cx("frame-div1")}>
+          <BldgInfoBlob {...usage_list} />
         </div>
-      </div>
-      <div className={cx("block", "bldg-info")}>
-        <div>
-          <p className={cx("keys")}>{keys.bldg_area}</p>
-          <p className={cx("vals")}>{vals.bldg_area}</p>
+        <div className={cx("frame-div4")}>
+          <BldgInfoText {...bldg_title} />
+          <div></div>
+          <BldgInfoText {...bldg_size} />
+          <BldgInfoText {...bldg_year} />
         </div>
-        <div>
-          <p className={cx("keys")}>{keys.bldg_size}</p>
-          <p className={cx("vals")}>{vals.bldg_size}</p>
+        <div className={cx("frame-div4")}>
+          <div></div>
+          <div></div>
+          <BldgInfoText {...bldg_floor_area_ratio} />
+          <BldgInfoText {...bldg_floor} />
         </div>
-        <div>
-          <p className={cx("keys")}>{keys.construction_year}</p>
-          <p className={cx("vals")}>{vals.construction_year}</p>
+        <div className={cx("frame-div4")}>
+          <div></div>
+          <div></div>
+          <BldgInfoText {...bldg_coverage_ratio} />
+          <BldgInfoText {...bldg_exclusive_rate} />
         </div>
-        <div>
-          <p className={cx("keys")}>{keys.far}</p>
-          <p className={cx("vals")}>{vals.far}</p>
-        </div>
-        <div>
-          <p className={cx("keys")}>{keys.bcr}</p>
-          <p className={cx("vals")}>{vals.bcr}</p>
-        </div>
-        <div>
-          <p className={cx("keys")}>{keys.par}</p>
-          <p className={cx("vals")}>{vals.par}</p>
+        <div className={cx("frame-div1")}>
+          <BldgInfoBlob {...bldg_usage_list} />
         </div>
       </div>
-      <div className={cx("block", "previous-sales")}>
-        <p className={cx("subtitle")}>과거 거래 사례</p>
-      </div>
-      <div className={cx("block", "construction")}>
-        <p className={cx("subtitle")}>개발 시</p>
-        <div>
-          <p className={cx("keys")}>{keys.construction.far}</p>
-          <p className={cx("vals")}>{vals.construction.far}</p>
+      <div className={cx("frame-section")}></div>
+      <div className={cx("frame-section")}>
+        <div className={cx("frame-div1")}>
+          <BldgInfoText {...official_price_title} />
         </div>
-        <div>
-          <p className={cx("keys")}>{keys.construction.bcr}</p>
-          <p className={cx("vals")}>{vals.construction.bcr}</p>
-        </div>
-        <div>
-          <p className={cx("keys")}>{keys.construction.height}</p>
-          <p className={cx("vals")}>{vals.construction.height}</p>
-        </div>
-        <div>
-          <p className={cx("keys")}>{keys.construction.use_area}</p>
-          {vals.construction.use_area.map((item) => {
-            return (
-              <p className={cx("vals")} key={item}>
-                {item}
-              </p>
-            );
-          })}
-        </div>
+        <div className={cx("frame-div2")}></div>
       </div>
     </div>
   );
 };
 
 BldgInfo.defaultProps = {
-  vals: {
-    name: "name",
-    addr1: "addr1",
-    addr2: "addr2",
-    buy_price: "buy_price",
-    buy_price_date: "buy_price_date",
-    noc: "noc",
-    land_area: "land_area",
-    use_area: "use_area",
-    bldg_area: "bldg_area",
-    bldg_size: "bldg_size",
-    construction_year: "construction_year",
-    far: "far",
-    bcr: "bcr",
-    par: "par",
-    construction: {
-      far: "construction.far",
-      bcr: "construction.bcr",
-      height: "construction.height",
-      use_area: ["[use_area]"],
+  price: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "최근 실거래가",
+      value: 200000000000,
+      value_unit: "\u00A0원",
+      base: "2019.03",
+      base_unit: "",
+      base_type: "string",
+      value_type: "number",
     },
+    style: "emph",
+    tooltip: [],
   },
-
-  keys: {
-    buy_price: "최근 실거래가",
-    noc: "건물 명목 NOC",
-    land_area: "토지면적",
-    use_area: "용도지구",
-    bldg_area: "건물면적",
-    bldg_size: "건물규모",
-    construction_year: "준공년도",
-    far: "용적률",
-    bcr: "건폐율",
-    par: "전용률",
-    construction: {
-      far: "상한용적률",
-      bcr: "상한건폐율",
-      height: "높이제한",
-      use_area: "토지이용계획",
+  noc: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "건물 명목 NOC",
+      value: 274000,
+      value_unit: "\u00A0원",
+      value_type: "number",
     },
+    style: "emph",
+    tooltip: ["건물 명목 NOC =", "(평당+평당 관리비)/전용률"],
+  },
+  land_price_per_area: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "토지 평당 단가",
+      value: 690000000,
+      value_unit: "\u00A0원",
+      value_type: "number",
+    },
+    style: "default",
+    tooltip: ["거래가(원)/토지면적(평)"],
+  },
+  rent: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "평당 임대료",
+      value: 111000,
+      value_unit: "\u00A0원",
+      value_type: "number",
+    },
+    style: "default",
+    tooltip: ["전체 월 임대료/전용면적(평)"],
+  },
+  opex: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "평당 관리비",
+      value: 48000,
+      value_unit: "\u00A0원",
+      value_type: "number",
+    },
+    style: "default",
+    tooltip: ["전체 월 관리료/전용면적(평)"],
+  },
+  land_title: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "토지",
+    },
+    style: "title",
+    tooltip: [],
+  },
+  land_size: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "면적",
+      value: 1179,
+      value_unit: "\u00A0[area]",
+      value_type: "number_detail",
+    },
+    style: "default",
+  },
+  land_usage: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "이용상황",
+      value: "업무용",
+      value_unit: "",
+      value_type: "string",
+    },
+    style: "default",
+  },
+  usage_list: {
+    component_type: "blob",
+    data: [
+      {
+        title: "서초로지구 지구단위계획",
+        tooltip: ["클릭시 토지계획이용원으로 이동"],
+        onClick: "토지계획이용원으로 링크",
+      },
+      {
+        title: "일반상업지역",
+        tooltip: ["클릭시 토지계획이용원으로 이동"],
+        onClick: "토지계획이용원으로 링크",
+      },
+    ],
+  },
+  attachment_land: {
+    component_type: "list",
+    data: {
+      title: "부속 필지",
+      value: [
+        [
+          {
+            value: "서울시 강남구 역삼동 101_1",
+            value_unit: "",
+            value_type: "string",
+          },
+          {
+            value: 3537,
+            value_unit: "\u00A0평",
+            value_type: "number_detail",
+          },
+        ],
+        [
+          {
+            value: "서울시 강남구 역삼동 101_1",
+            value_unit: "",
+            value_type: "string",
+          },
+          {
+            value: 3537,
+            value_unit: "\u00A0평",
+            value_type: "number_detail",
+          },
+        ],
+      ],
+    },
+    dropdown_from: 2,
+  },
+  bldg_title: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "건물1",
+      base: "강욱 빌딩",
+      base_type: "string",
+    },
+    style: "title",
+    tooltip: [],
+  },
+  bldg_size: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "연면적",
+      value: 4919,
+      value_unit: "\u00A0평",
+      value_type: "number_detail",
+    },
+    style: "default",
+  },
+  bldg_year: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "준공년도",
+      value: "2019",
+      value_unit: "년",
+      value_type: "string",
+    },
+    style: "default",
+  },
+  bldg_floor_area_ratio: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "용적률",
+      value: 4.59,
+      value_unit: "\u00A0%",
+      value_type: "rate_over",
+    },
+    style: "default",
+  },
+  bldg_floor: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "건물 규모",
+      value: [-5, 20],
+      value_unit: "",
+      value_type: "floor_range",
+    },
+    style: "default",
+  },
+  bldg_coverage_ratio: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "건폐율",
+      value: 0.47,
+      value_unit: "\u00A0%",
+      value_type: "rate",
+    },
+    style: "default",
+  },
+  bldg_exclusive_rate: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "전용률",
+      value: 0.47,
+      value_unit: "\u00A0%",
+      value_type: "rate",
+    },
+    style: "default",
+  },
+  bldg_usage_list: {
+    component_type: "blob",
+    data: [
+      {
+        title: "업무시설",
+        tooltip: [],
+      },
+      {
+        title: "1종근린생활시설",
+        tooltip: [],
+      },
+      {
+        title: "교육연구",
+        tooltip: [],
+      },
+    ],
+  },
+  bldg_datail: {
+    component_type: "list",
+    data: {
+      title: "건물 상세",
+      value: [
+        [
+          {
+            value: 4,
+            value_unit: "F",
+            value_type: "number_detail",
+          },
+          {
+            value: "업무시설, 1종근린생활시설",
+            value_unit: "",
+            value_type: "string",
+          },
+          {
+            value: "78",
+            value_unit: "평",
+            value_type: "number_detail",
+          },
+        ],
+        [
+          {
+            value: 3,
+            value_unit: "F",
+            value_type: "number_detail",
+          },
+          {
+            value: "1종근린생활시설, 교육연구",
+            value_unit: "",
+            value_type: "string",
+          },
+          {
+            value: 56,
+            value_unit: "평",
+            value_type: "number_detail",
+          },
+        ],
+      ],
+    },
+    dropdown_from: 2,
+  },
+  dev_title: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "개발 가능 한계",
+    },
+    style: "title",
+    tooltip: [
+      "서초로지구 지구단위계획에 의하였으며,",
+      "일반상업지역에 적용되는 개발한계임",
+    ],
+  },
+  dev_floor_area_ratio: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "최대 용적률",
+      value: 8,
+      value_unit: "\u00A0%",
+      value_type: "rate_over",
+    },
+    style: "default",
+  },
+  dev_coverage_ratio: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "최대 건폐율",
+      value: 0.6,
+      value_unit: "\u00A0%",
+      value_type: "rate",
+    },
+    style: "default",
+  },
+  dev_height: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "높이 제한",
+      value: 70,
+      value_unit: "\u00A0m",
+      value_type: "number_detail",
+    },
+    style: "default",
+  },
+  dev_usage_list: {
+    component_type: "blob",
+    data: [
+      {
+        title: "서초로지구 지구단위계획",
+        tooltip: ["클릭시 토지계획이용원으로 이동"],
+        onClick: "토지계획이용원으로 링크",
+      },
+      {
+        title: "일반상업지역",
+        tooltip: ["클릭시 토지계획이용원으로 이동"],
+        onClick: "토지계획이용원으로 링크",
+      },
+    ],
+  },
+  official_price_title: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "공시지가",
+    },
+    style: "title",
+  },
+  official_price_year: {
+    component_type: "list",
+    show: true,
+    data: {
+      title: "기준년도",
+      value: [
+        [
+          {
+            value: "2021.01",
+            value_unit: "",
+            value_type: "string",
+          },
+        ],
+        [
+          {
+            value: "2020.01",
+            value_unit: "",
+            value_type: "string",
+          },
+        ],
+        [
+          {
+            value: "2019.01",
+            value_unit: "",
+            value_type: "string",
+          },
+        ],
+      ],
+    },
+    dropdown_from: 3,
+  },
+  official_price: {
+    component_type: "list",
+    show: true,
+    data: {
+      title: "공시지가",
+      value: [
+        [
+          {
+            value: 121500000,
+            value_unit: "\u00A0원/평",
+            value_type: "number",
+          },
+        ],
+        [
+          {
+            value: 113200000,
+            value_unit: "\u00A0원/평",
+            value_type: "number",
+          },
+        ],
+        [
+          {
+            value: 92300000,
+            value_unit: "\u00A0원/평",
+            value_type: "number",
+          },
+        ],
+      ],
+    },
+    dropdown_from: 3,
+  },
+  siml_land_title: {
+    component_type: "text",
+    show: true,
+    data: {
+      title: "유사 매물",
+    },
+    style: "title",
+  },
+  siml_land_address: {
+    component_type: "list",
+    show: true,
+    data: {
+      title: "주소",
+      value: [
+        [
+          {
+            icon: "bookmark",
+          },
+          {
+            value: "서초구 서초동 321",
+            value_unit: "",
+            value_type: "string",
+          },
+        ],
+        [
+          {
+            icon: "bookmark",
+          },
+          {
+            value: "강남구 역삼동 123",
+            value_unit: "",
+            value_type: "string",
+          },
+        ],
+        [
+          {
+            icon: "bookmark",
+          },
+          {
+            value: "강남구 역삼동 456",
+            value_unit: "",
+            value_type: "string",
+          },
+        ],
+      ],
+    },
+    dropdown_from: 3,
+  },
+  siml_land_price: {
+    component_type: "list",
+    show: true,
+    data: {
+      title: "최근 실거래가",
+      value: [
+        [
+          {
+            value: 2630000,
+            value_unit: "\u00A0원/평",
+            value_type: "number_detail",
+          },
+        ],
+        [
+          {
+            value: 2830000,
+            value_unit: "\u00A0원/평",
+            value_type: "number_detail",
+          },
+        ],
+        [
+          {
+            value: 2310000,
+            value_unit: "\u00A0원/평",
+            value_type: "number_detail",
+          },
+        ],
+      ],
+    },
+    dropdown_from: 3,
   },
 };
 
