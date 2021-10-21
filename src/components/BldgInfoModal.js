18f1e85d1920f568d365545c8400951fbd805f49
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-21 15:37:49 +0000

    structure modified

--- src/components/BldgInfoModal.js
+++ src/components/BldgInfoModal.js
@@ -8,24 +8,41 @@ import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-const BldgInfoModal = ({ info, handleBldgInfo, setIsClicked }) => {
+const BldgInfoModal = ({
+  title,
+  sub_title,
+  info,
+  handleBldgInfo,
+  setIsClicked,
+}) => {
   if (info.active === false) {
     return <></>;
   } else {
     return (
       <div className={cx("wrapper")}>
         <div className={cx("modal")}>
-          <BldgInfo keys={info.keys} vals={info.vals} />
-          <CloseSvg
-            className={cx("btn-close")}
-            onClick={() => {
-              handleBldgInfo({ show: false, id: -1 });
-              setIsClicked(true);
-            }}
-          />
-          <button className={cx("box-btn")} type="submit" value="좌표">
+          <div className={cx("frame-title")}>
+            <div className={cx("frame-left")}>save</div>
+            <div className={cx("frame-center")}>
+              <div className={cx("title")}>{title}</div>
+              <div className={cx("sub-title")}>{sub_title}</div>
+            </div>
+            <div className={cx("frame-right")}>
+              <CloseSvg
+                className={cx("btn-close")}
+                onClick={() => {
+                  handleBldgInfo({ show: false, id: -1 });
+                  setIsClicked(true);
+                }}
+              />
+            </div>
+          </div>
+          <div className={cx("frame-content")}>
+            <BldgInfo keys={info.keys} vals={info.vals} />
+          </div>
+          <button className={cx("frame-btn")} type="submit" value="좌표">
             <Link to="/valuation">
-              <div className={cx("box")}>개발 Valuation 검토</div>
+              <div className={cx("box")}>개발 밸류에이션 검토</div>
             </Link>
           </button>
         </div>
@@ -35,6 +52,8 @@ const BldgInfoModal = ({ info, handleBldgInfo, setIsClicked }) => {
 };
 
 BldgInfoModal.defaultProps = {
+  title: "서울시 강남구 역삼동 101-1",
+  sub_title: "서울시 강남구 강남로 1",
   info: { active: true },
   handleBldgInfo: (_) => {},
   setIsClicked: (_) => {},
