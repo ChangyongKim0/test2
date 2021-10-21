628e4b7458b7d1206880fcbe5345159be0b5fbe9
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-21 15:35:46 +0000

    autocomplete by kako api created

--- src/components/AddressSearcher.js
+++ src/components/AddressSearcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useReducer } from "react";
 // , { useEffect }
 
 import styles from "./AddressSearcher.module.scss";
@@ -7,16 +7,169 @@ import UseAutocomplete from "./UseAutoComplete";
 
 const cx = classNames.bind(styles);
 
+let input_search = "";
+
+const reduceSearchResults = (state, action) => {
+  let new_state = { ...state };
+  switch (action.type) {
+    case "focus_down":
+      if (state.focus < state.data.length - 1) {
+        new_state.focus = state.focus + 1;
+      } else {
+        new_state.focus = 0;
+      }
+      return new_state;
+    case "focus_up":
+      if (state.focus > 0) {
+        new_state.focus = state.focus - 1;
+      } else {
+        new_state.focus = Math.max(state.data.length - 1, 0);
+      }
+      return new_state;
+    case "update":
+      new_state.data = action.data;
+      new_state.focus = 0;
+      return new_state;
+    default:
+      return state;
+  }
+};
+
+const emphasizeText = (text, word) => {
+  // return text.replace(new RegExp(word, "gi"), `<mark>${word}</mark>`);
+  return text.split(word).map((e, idx) =>
+    idx == 0 ? (
+      <span>{e}</span>
+    ) : (
+      <span>
+        <span className={cx("text-emph")}>{word}</span>
+        {e}
+      </span>
+    )
+  );
+};
+
+const formatCategory = (data) => {
+  const splitted = data.split(" > ");
+  if (splitted.length > 1) {
+    return splitted[0] + " > " + splitted.slice(-1);
+  } else {
+    return data;
+  }
+};
+
 const AddressSearcher = () => {
+  const [search_results, handleSearchResults] = useReducer(
+    reduceSearchResults,
+    { data: [], focus: 0 }
+  );
+
+  let places = new window.kakao.maps.services.Places();
+
+  let callback = function (result, status) {
+    if (status === window.kakao.maps.services.Status.OK) {
+      console.log(result);
+      handleSearchResults({ type: "update", data: result });
+    }
+  };
+
+  // address_name: "서울 마포구 연남동 568-23";
+  // category_group_code: "FD6";
+  // category_group_name: "음식점";
+  // category_name: "음식점 > 한식";
+  // distance: "";
+  // id: "1650833864";
+  // phone: "02-332-9357";
+  // place_name: "한";
+  // place_url: "http://place.map.kakao.com/1650833864";
+  // road_address_name: "서울 마포구 동교로27길 41";
+  // x: "126.921453250265";
+  // y: "37.5596821334816";
+
+  useEffect(() => {
+    input_search = document.getElementById("input_search");
+    input_search.addEventListener("keydown", (e) => {
+      if (e.key == "ArrowUp") {
+        e.preventDefault();
+      }
+    });
+    input_search.addEventListener("keyup", (e) => {
+      console.log(e);
+      if (e.code == "ArrowUp") {
+        if (e.key == "ArrowUp") {
+          handleSearchResults({ type: "focus_up" });
+        }
+      } else if (e.code == "ArrowDown") {
+        if (e.key == "ArrowDown") {
+          handleSearchResults({ type: "focus_down" });
+        }
+      } else if (e.code != "ArrowLeft" && e.code != "ArrowRight") {
+        places.keywordSearch(input_search.value, callback);
+      }
+    });
+  }, []);
+
+  const forceSearch = () => {
+    places.keywordSearch(input_search.value, callback);
+  };
+
   return (
     <div className={cx("wrapper")}>
-      {/* <input
-        className={cx("text-field")}
-        type="text"
-        placeholder="주소 입력"
-      ></input> */}
-      <UseAutocomplete />
-      <div className={cx("icon")}>O.</div>
+      <div className={cx("frame-search")}>
+        <input
+          id="input_search"
+          className={cx("text-field")}
+          type="text"
+          placeholder="주소 입력"
+          onFocus={() => {
+            forceSearch();
+          }}
+          onBlur={() => {
+            handleSearchResults({ type: "update", data: [] });
+          }}
+        ></input>
+        {/* <UseAutocomplete /> */}
+        <div className={cx("icon")}>O.</div>
+      </div>
+      {search_results.data.length > 0 ? (
+        <div className={cx("drop-down")}>
+          {search_results.data.map((e, idx) => (
+            <div
+              key={idx}
+              id={"input_search_" + idx}
+              tabIndex="-1"
+              className={cx(
+                "frame-list",
+                search_results.focus == idx ? "focused" : ""
+              )}
+            >
+              <div className={cx("title")}>
+                {emphasizeText(e.address_name, input_search.value)}
+              </div>
+              <div className={cx("sub-title")}>
+                {e.road_address_name == ""
+                  ? "-"
+                  : emphasizeText(e.road_address_name, input_search.value)}
+              </div>
+              <div className={cx("frame-value")}>
+                <div className={cx("text-bold")}>
+                  {e.place_name == ""
+                    ? "-"
+                    : emphasizeText(e.place_name, input_search.value)}
+                </div>
+                <div className={cx("text")}>
+                  {emphasizeText(
+                    formatCategory(e.category_name),
+                    input_search.value
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
