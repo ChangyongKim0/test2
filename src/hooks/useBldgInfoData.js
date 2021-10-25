7f58dd7a5dac490e8288250693440422d09eb9e3
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-25 09:22:14 +0000

    sync cookie, server, state

--- src/hooks/useBldgInfoData.js
+++ src/hooks/useBldgInfoData.js
@@ -8,7 +8,7 @@ import React, {
 import calculateValuation from "../data/calculateValuation";
 import naked_data from "../data/naked_data";
 import { cloneDeep } from "lodash";
-import useCookieData from "../hooks/useCookieData";
+import useCookieData, { getCookie, setCookie } from "../hooks/useCookieData";
 
 const base_data = {};
 
@@ -28,10 +28,20 @@ const reduceBldgInfoData = (state, action) => {
       new_state.lat = action.lat || state.lat;
       new_state.lng = action.lng || state.lng;
       new_state.data = action.data || state.data;
+      new_state.level = action.level || state.level;
       new_state.handleCookieData({
         type: "patch",
-        data: { pnu: new_state.pnu, lat: action.lat, lng: action.lng },
+        data: {
+          pnu: new_state.pnu,
+          lat: new_state.lat,
+          lng: new_state.lng,
+          level: new_state.level,
+        },
       });
+      setCookie("pnu", new_state.pnu, 365);
+      setCookie("level", new_state.level, 365);
+      setCookie("lat", new_state.lat, 365);
+      setCookie("lng", new_state.lng, 365);
       return new_state;
     case "update":
       new_state.pnu = action.pnu;
@@ -40,8 +50,28 @@ const reduceBldgInfoData = (state, action) => {
         type: "patch",
         data: { pnu: action.pnu },
       });
+      setCookie("pnu", new_state.pnu, 365);
       return new_state;
-
+    case "location_update":
+      new_state.pnu = action.pnu || state.pnu;
+      new_state.lat = action.lat || state.lat;
+      new_state.lng = action.lng || state.lng;
+      new_state.level = action.level || state.level;
+      // new_state.handleCookieData({
+      //   type: "patch",
+      //   data: {
+      //     pnu: new_state.pnu,
+      //     lat: new_state.lat,
+      //     lng: new_state.lng,
+      //     level: new_state.level,
+      //   },
+      // });
+      // 위를 실행할 경우 서버 부하가 커지므로 클라이언트 측에서 처리하도록
+      setCookie("pnu", new_state.pnu, 365);
+      setCookie("level", new_state.level, 365);
+      setCookie("lat", new_state.lat, 365);
+      setCookie("lng", new_state.lng, 365);
+      return new_state;
     default:
       return state;
   }
@@ -57,24 +87,24 @@ export const BldgInfoDataProvider = ({ children }) => {
   const [bldg_info_data, handleBldgInfoData] = useReducer(reduceBldgInfoData, {
     handleCookieData: handleCookieData,
     move_toggle: true,
-    pnu: "1168010100108080005",
-    lat: 37.497928,
-    lng: 127.027583,
-    level: 3,
+    pnu: getCookie("pnu") || "1168010100108080005",
+    lat: getCookie("lat") || 37.497928,
+    lng: getCookie("lng") || 127.027583,
+    level: getCookie("level") || 3,
     data: {},
   });
-  useEffect(() => {
-    if (cookie_data.data_updated) {
-      console.log(cookie_data);
-      handleBldgInfoData({
-        type: "move_update",
-        pnu: cookie_data.data.pnu,
-        lat: cookie_data.data.lat,
-        lng: cookie_data.data.lng,
-        level: cookie_data.data.level,
-      });
-    }
-  }, [cookie_data.data_updated]);
+  // useEffect(() => {
+  //   if (cookie_data.data_updated) {
+  //     console.log(cookie_data);
+  //     handleBldgInfoData({
+  //       type: "move_update",
+  //       pnu: cookie_data.data.pnu,
+  //       lat: cookie_data.data.lat,
+  //       lng: cookie_data.data.lng,
+  //       level: cookie_data.data.level,
+  //     });
+  //   }
+  // }, [cookie_data.data_updated]);
   const value = useMemo(() => {
     console.log("memo rewritten.");
     return { bldg_info_data, handleBldgInfoData };
