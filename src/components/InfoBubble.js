4144229446b6fb666d5fb41113be4d06386ddfc9
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-08 16:31:18 +0000

    use bookmark

--- src/components/InfoBubble.js
+++ src/components/InfoBubble.js
@@ -4,19 +4,35 @@ import { col_primary, col_w } from "../util/style";
 import styles from "./InfoBubble.module.scss";
 import classNames from "classnames/bind";
 import useUnitType from "../hooks/useUnitType";
+import BookMarkButton from "../atom/BookMarkButton";
 
 const cx = classNames.bind(styles);
 
-const InfoBubble = ({ id, data, onClick }) => {
+const InfoBubble = ({ id, data, onClick, is_saved, tr_exists }) => {
   return (
     <div className={cx("wrapper")}>
       <div id={id} className={cx("info-bubble")}>
-        <InfoBubbleSvg className={cx("info-bubble-svg")} />
-        <div className={cx("frame")}>
-          <div className={cx("title")}>{data.price}</div>
-          <div>{data.date}</div>
-          <div className={"info-bubble-unit-field"}>{data.price_per_py}</div>
-        </div>
+        {tr_exists ? (
+          <>
+            <InfoBubbleSvg className={cx("info-bubble-svg")} />
+            <div className={cx("frame")}>
+              <div className={cx("title")}>{data.price}</div>
+              <div>{data.date}</div>
+              <div className={"info-bubble-unit-field"}>
+                {data.price_per_py}
+              </div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        {is_saved ? (
+          <div className={cx("info-bookmark")}>
+            <BookMarkButton />
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
@@ -26,6 +42,8 @@ InfoBubble.defaultProps = {
   id: 1000,
   data: { price: "price", date: "date", price_per_py: "price_per_py" },
   onClick: () => {},
+  is_saved: true,
+  tr_exists: true,
 };
 
 export default InfoBubble;
