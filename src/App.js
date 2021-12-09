be647174cf94efbbf2d85c6f5bc8d72b54bc76e7
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-09 08:51:46 +0000

    overlay reloader added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import { ZIndexProvider } from "./functions/Zindexer";
 import { BldgInfoDataProvider } from "./hooks/useBldgInfoData";
 import { CookieDataProvider } from "./hooks/useCookieData";
+import { OverlayReloaderProvider } from "./hooks/useOverlayReloader";
 import { UnitTypeProvider } from "./hooks/useUnitType";
 import { ValuationCalculatorProvider } from "./hooks/useValuationCalculator";
 import {
@@ -18,25 +19,27 @@ class App extends Component {
   render() {
     return (
       <CookieDataProvider>
-        <UnitTypeProvider>
-          <BldgInfoDataProvider>
-            <ValuationCalculatorProvider>
-              <ZIndexProvider>
-                {/* <Route exact path="/" component={Home} /> */}
-                <Route path="/about" component={About} />
-                <Route exact path="/" component={Home} />
-                {/* <Route path="/login" component={Login} />
+        <OverlayReloaderProvider>
+          <UnitTypeProvider>
+            <BldgInfoDataProvider>
+              <ValuationCalculatorProvider>
+                <ZIndexProvider>
+                  {/* <Route exact path="/" component={Home} /> */}
+                  <Route path="/about" component={About} />
+                  <Route exact path="/" component={Home} />
+                  {/* <Route path="/login" component={Login} />
         <Route path="/map" component={Map} />
         <Route exact path="/tips" component={Tips} />
         <Route path="/tips/new" component={NewTips} /> */}
-                <Route exact path="/valuation" component={Valuation} />
-                <Route path="/valuation/comp" component={ValuationComp} />
-                <Route path="/testpage" component={TestPage} />
-                <Route path="/map/comp" component={MapComp} />
-              </ZIndexProvider>
-            </ValuationCalculatorProvider>
-          </BldgInfoDataProvider>
-        </UnitTypeProvider>
+                  <Route exact path="/valuation" component={Valuation} />
+                  <Route path="/valuation/comp" component={ValuationComp} />
+                  <Route path="/testpage" component={TestPage} />
+                  <Route path="/map/comp" component={MapComp} />
+                </ZIndexProvider>
+              </ValuationCalculatorProvider>
+            </BldgInfoDataProvider>
+          </UnitTypeProvider>
+        </OverlayReloaderProvider>
       </CookieDataProvider>
     );
   }
