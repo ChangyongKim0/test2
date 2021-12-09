0315097598cb7815669390b34061a796127f2535
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-09 08:25:16 +0000

    use overlay reloader and bookmark linked

--- src/components/BldgInfoModal.js
+++ src/components/BldgInfoModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BldgInfo from "./BldgInfo";
 import { ReactComponent as CloseSvg } from "../atom/CloseSvg.svg";
 
@@ -7,6 +7,8 @@ import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
 import useBldgInfoData from "../hooks/useBldgInfoData";
 import BookMarkButton from "../atom/BookMarkButton";
+import useCookieData from "../hooks/useCookieData";
+import useOverlayReloader from "../hooks/useOverlayReloader";
 
 const cx = classNames.bind(styles);
 
@@ -17,7 +19,38 @@ const BldgInfoModal = ({
   handleBldgInfo,
   setIsClicked,
 }) => {
+  const [cookie_data, handleCookieData] = useCookieData();
   const [bldg_info_data, _] = useBldgInfoData();
+  const [_1, handleOverlayReloader] = useOverlayReloader();
+
+  const isBookMarked = (pnu, pilji_list) => {
+    console.log(pilji_list, pnu);
+    if (pilji_list != undefined) {
+      return pilji_list.map((e) => e.id).includes(pnu);
+    } else {
+      return false;
+    }
+  };
+
+  const toggleBookmarkedPiljiData = (bldg_info_data, cookie_data) => {
+    if (isBookMarked(bldg_info_data.pnu, cookie_data.data.pilji_list)) {
+      handleCookieData({
+        type: "delete",
+        data: {
+          pilji_list: bldg_info_data.pnu,
+        },
+      });
+    } else {
+      handleCookieData({
+        type: "add",
+        data: {
+          pilji_list: { data: bldg_info_data.data, id: bldg_info_data.pnu },
+        },
+      });
+    }
+    handleOverlayReloader({ type: "activate" });
+  };
+
   useEffect(() => {
     console.log(bldg_info_data);
   }, [bldg_info_data]);
@@ -31,7 +64,15 @@ const BldgInfoModal = ({
           <div className={cx("frame-title")}>
             <div className={cx("frame-left")}>
               <div>
-                <BookMarkButton />
+                <BookMarkButton
+                  is_saved={isBookMarked(
+                    bldg_info_data.pnu,
+                    cookie_data.data.pilji_list
+                  )}
+                  onClick={() => {
+                    toggleBookmarkedPiljiData(bldg_info_data, cookie_data);
+                  }}
+                />
               </div>
             </div>
             <div className={cx("frame-center")}>
