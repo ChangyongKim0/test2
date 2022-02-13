0824d6105f6cc23a6120cb3f817e0e8f59d1b888
ChangyongKim0 <changyong.kim.0@gmail.com>
2022-02-13 15:59:46 +0000

    updating base data option created

--- src/hooks/useValuationCalculator.js
+++ src/hooks/useValuationCalculator.js
@@ -11,6 +11,29 @@ export const ValuationContext = createContext({
   setValuationData: () => {},
 });
 
+const _formatFloorRange = (start, end) => {
+  return _formatFloor(start) + " / " + _formatFloor(end);
+}
+
+const _formatFloor = (number) => {
+  if(typeof number != typeof 1){
+    return "-"
+  } else if(number <= 0){
+    return "B" + (1-number);
+  } else{
+    return number + "F";
+  }
+}
+
+// const _formatUsageList = (list) => {
+//   if(list.length > 2){
+//     return list.slice(0,2).join(", ") + "..."
+//   }
+//   else {
+//     return list.join(", ")
+//   }
+// }
+
 const reduceValuationData = (state, action) => {
   let new_state = cloneDeep(state);
   // console.log(new_state);
@@ -18,6 +41,34 @@ const reduceValuationData = (state, action) => {
   switch (action.type) {
     case "create":
       return calculateValuation(action.data, action.id);
+    case "update base data":
+      console.log("UPDATE");
+      console.log(action.data);
+      if(action.data.addr != undefined){
+        new_state.base.land_area.value = action.data.land.area;
+        new_state.base.use_area.value = action.data.land.usage_list.join(", ");
+        new_state.base.recent_trade.base = action.data.transaction.date || "";
+        new_state.base.recent_trade.value = action.data.transaction.price || "-";
+        new_state.base.land_price_per_area.value = action.data.land.price_per_area || "-";
+        new_state.base.noc.value = "-";
+        new_state.base.opex.value = ["-", "-"];
+        if(action.data.bldg_exists){
+          const bldg_info = action.data.bldg.bldg_info_list[action.data.bldg_idx]
+        new_state.base.floor_area.value = bldg_info.farea;
+        new_state.base.far.value = bldg_info.far;
+        new_state.base.bcr_par.value = [bldg_info.bcr, bldg_info.par];
+        new_state.base.size.value = _formatFloorRange(bldg_info.size[0], bldg_info.size[1]);
+        new_state.base.const_year.value = bldg_info.const_year;
+        }
+        else{
+        new_state.base.floor_area.value = "-";
+        new_state.base.far.value = "-";
+        new_state.base.bcr_par.value = ["-", "-"];
+        new_state.base.size.value = _formatFloorRange("-","-");
+        new_state.base.const_year.value = "-";
+        }
+      }
+      return calculateValuation(new_state, action.id);
     case "update":
       if (action.value) {
         if (action.id.includes(".")) {
