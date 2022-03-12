f86aba7e59cb895ee5e4072735fdb758142fac21
ChangyongKim0 <cyklone001@gmail.com>
2022-03-12 19:23:56 +0000

    proxy change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const { IP_PORT } = require("./src_shortcut");
 
 module.exports = function (app) {
-  app.use("/api", createProxyMiddleware({ target: "http://localhost:3001" }));
+  // app.use("/api", createProxyMiddleware({ target: "http://localhost:3001" }));
+  app.use("/api", createProxyMiddleware({ target: IP_PORT, changeOrigin: true}));
 };
